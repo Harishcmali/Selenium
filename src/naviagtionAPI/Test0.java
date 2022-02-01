318ab865a22e726684493dcd58a9c31811e24093
QSP <QSP@QSP>
2022-02-01 18:52:15 +0530

    Selenium

---
+++ src/naviagtionAPI/Test0.java
@@ -0,0 +1,33 @@
+package naviagtionAPI;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test0 {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+		Thread.sleep(3000);
+		driver.navigate().to("https://www.amazon.com/");
+		
+		
+		Thread.sleep(3000);
+		//to back
+		driver.navigate().back();
+		
+		
+		Thread.sleep(3000);
+		//to forward
+		driver.navigate().forward();
+		
+		Thread.sleep(3000);
+		//to refresh
+		driver.navigate().refresh();
+		
+
+	}
+
+}
