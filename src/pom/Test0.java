7030978a12b3a3993549bb33db90d85f618c0fd3
QSP <QSP@QSP>
2022-02-18 19:01:48 +0530

    Pom

---
+++ src/pom/Test0.java
@@ -0,0 +1,27 @@
+package pom;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test0 {
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+	//	WebElement serach = driver.findElement(By.name("q"));
+		Googlepage g=new Googlepage(driver);
+		
+		driver.navigate().refresh();
+	
+		//	serach.sendKeys("phone");
+		g.serachtb("phone");
+		
+	}
+
+}
